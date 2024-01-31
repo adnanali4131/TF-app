1aa87e6260eecfcff5e00865276137cf780b90bf
adnanali4131 <adnanali4131@gmail.com>
2024-01-31 16:48:50 +0500

    login add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/home';
+import SignUp from './components/auth/signUp';
 import Login from './components/auth/login';
 import "./App.css";
 
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/signup" element={<SignUp />} /> */}
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Fragment>
     </Router>
--- src/components/auth/signUp.js
+++ src/components/auth/signUp.js
@@ -1,8 +1,9 @@
 import React from "react";
 import './index.css';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-const Login = () => {
+const SignUp = () => {
+
 
   return (
     <>
@@ -19,6 +20,14 @@ const Login = () => {
                 </h1>
                 <form className="mb-3">
                   <div>
+                    <label htmlFor="firstName" className="form-label">Your first Name</label>
+                    <input type="firstName" name="firstName" id="firstName" className="form-control" placeholder="First Name" required />
+                  </div>
+                  <div>
+                    <label htmlFor="lastName" className="form-label">Your lastName</label>
+                    <input type="lastName" name="lastName" id="lastName" className="form-control" placeholder="Last Name" required />
+                  </div>
+                  <div>
                     <label htmlFor="email" className="form-label">Your email</label>
                     <input type="email" name="email" id="email" className="form-control" placeholder="name@example.com" required />
                   </div>
@@ -26,16 +35,10 @@ const Login = () => {
                     <label htmlFor="password" className="form-label">Password</label>
                     <input type="password" name="password" id="password" className="form-control" placeholder="••••••••" required />
                   </div>
-                  <div className="d-flex pt-4 justify-content-between">
-                    <div className="form-check">
-                      <input id="remember" type="checkbox" className="form-check-input" required />
-                      <label htmlFor="remember" className="form-check-label">Remember me</label>
-                    </div>
-                    <a href="#" className="text-decoration-none">Forgot password?</a>
-                  </div>
-                  <button type="submit" className="w-100 btn btn-primary mt-3">Sign in</button>
-                  <p to="/signup" className="mt-2 text-muted">
-                    Don’t have an account yet? <Link to="/signup" className="text-decoration-none">Sign up</Link>
+
+                  <button type="submit" className="w-100 btn btn-primary mt-3">Sign Up</button>
+                  <p className="mt-2 text-muted">
+                    Already have an account  <Link to="/login" className="text-decoration-none">Login</Link>
                   </p>
                 </form>
               </div>
@@ -47,7 +50,7 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
 
 
 
